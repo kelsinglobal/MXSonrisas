dfafb29ca1e9bc0beea3882043f883ed580952bd
kelsinglobal <92062739+kelsinglobal@users.noreply.github.com>
2022-07-25 18:10:39 -0500

    easi_areas_attention

---
+++ easi_areas_attention/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Easi manage area attention",
+    'summary': """
+        Manage area attention""",
+    'description': """
+       Manage area attention
+    """,
+    'author': "",
+    'website': "",
+    'category': '',
+    'version': '1.0',
+    'depends': ['easi_medical_center'],
+    'data': [
+        'views/area_attention_views.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
