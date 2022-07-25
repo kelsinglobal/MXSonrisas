40072ea65acfbc1227064596bfaf14357f335e6b
kelsinglobal <92062739+kelsinglobal@users.noreply.github.com>
2022-07-25 18:10:20 -0500

    easi_medical_center

---
+++ easi_medical_center/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Easi manage medical center",
+    'summary': """
+        Manage medical center""",
+    'description': """
+       Manage medical center
+    """,
+    'author': "",
+    'website': "",
+    'category': '',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'views/medical_center_views.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
